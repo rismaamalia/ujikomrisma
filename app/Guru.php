1c6e0692906c75243c4115e6b6110c0b296533a4
rismaamalia <rismaamalia546@gmail.com>
2019-08-07 17:12:14 +0700

    first commit

--- app/Guru.php
+++ app/Guru.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guru extends Model
 {
-    //
+    Protected $fillable = ['guru_kode','kompetensi_kode','guru_nip','guru_nama','guru_alamat','guru_tlpn'];
+    Public $timestamps = true;
+ 
+    public function kompetensi_keahlian()
+    {
+    	return $this->belongsTo('App\Kompetensi_keahlian');
+}
 }
